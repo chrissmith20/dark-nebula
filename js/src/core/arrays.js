245c59937f737b251c79d74462b681596cd98f49
chrissmith20 <christopher.v.smith20@gmail.com>
2020-09-28 12:52:25 -0400

    initial commit

---
+++ js/src/core/arrays.js
@@ -0,0 +1,39 @@
+append = (arr, item) => {
+
+};
+
+truncate = (arr) => {
+
+};
+
+prepend = (arr, item) => {
+
+};
+
+curtail = (arr) => {
+
+};
+
+concat = (arr1, arr2) => {
+
+};
+
+insert = (arr, item, index) => {
+
+}
+
+count = (arr, item) => {
+
+};
+
+duplicates = (arr) => {
+
+};
+
+square = (arr) => {
+//make use of .map()
+};
+
+findAllOccurrences = (arr, item) => {
+
+};
